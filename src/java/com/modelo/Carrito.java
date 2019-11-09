50773fa2cf10aa659c03ba546ee1a4122cb023d7
SERVER <SERVER@SERVER-PC>
2019-11-09 11:40:33 -0600

    Agregue modelo carrito

---
+++ src/java/com/modelo/Carrito.java
@@ -0,0 +1,19 @@
+
+package com.modelo;
+
+/**
+ * Nombre de la clase: Carrito
+ * Fecha: 09-11-2019
+ * Version: 1.0
+ * Copyriht: Denis Valladares
+ * @author Denis Valladares
+ */
+public class Carrito {
+    private int idLibro;
+    private String nombre;
+    private String editorial;
+    private String autor;
+    private int cantidad;
+    private double precio;
+    private double subtotal;
+}
