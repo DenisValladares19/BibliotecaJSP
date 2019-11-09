db08b3850a3b1f7eca613b721886c0cb6cb21729
SERVER <SERVER@SERVER-PC>
2019-11-09 11:42:45 -0600

    Agregue modelo carrito

--- src/java/com/modelo/Carrito.java
+++ src/java/com/modelo/Carrito.java
@@ -16,11 +16,12 @@ public class Carrito {
     private int cantidad;
     private double precio;
     private double subtotal;
+    private int idCliente;
 
     public Carrito() {
     }
 
-    public Carrito(int idLibro, String nombre, String editorial, String autor, int cantidad, double precio, double subtotal) {
+    public Carrito(int idLibro, String nombre, String editorial, String autor, int cantidad, double precio, double subtotal, int idCliente) {
         this.idLibro = idLibro;
         this.nombre = nombre;
         this.editorial = editorial;
@@ -28,6 +29,7 @@ public class Carrito {
         this.cantidad = cantidad;
         this.precio = precio;
         this.subtotal = subtotal;
+        this.idCliente = idCliente;
     }
 
     public int getIdLibro() {
@@ -85,4 +87,12 @@ public class Carrito {
     public void setSubtotal(double subtotal) {
         this.subtotal = subtotal;
     }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(int idCliente) {
+        this.idCliente = idCliente;
+    }   
 }
