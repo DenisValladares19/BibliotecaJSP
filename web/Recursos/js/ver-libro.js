eb8c407236a562aa99f253e4ec6d9ac8206cf584
Admin <Admin@Admin-PC>
2019-11-11 18:41:58 -0600

    agregue funcion para recargar pagina en cierto tiempo

--- web/Recursos/js/ver-libro.js
+++ web/Recursos/js/ver-libro.js
@@ -11,24 +11,26 @@ $(document).ready(function() {
                 var idLibro = $("#idLibro").val();
                 var cant = $("#cant").val();
                 var accion = "agregar";
-                $.post("carro",{
-                    idLibro,
-                    cant,
-                    accion
-                },function(res){
-                    const Toast = Swal.mixin({
-                        toast: true,
-                        position: 'top-end',
-                        showConfirmButton: false,
-                        timer: 2000
-                      });
+                window.setInterval(
+                    $.post("carro",{
+                        idLibro,
+                        cant,
+                        accion
+                    },function(res){
+                        const Toast = Swal.mixin({
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 2000
+                          });
 
-                      Toast.fire({
-                        icon: 'success',
-                        title: res
-                      });  
-                      location.reload();
-                });
+                          Toast.fire({
+                            icon: 'success',
+                            title: res
+                          });  
+                          location.reload();
+                    }),"2000"    
+                );
                 $("#cant").val(1);
            }
             
